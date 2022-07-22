0146e168927889a2e66c62eb05a46424c627ce4a
colinbrust <colin.brust@gmail.com>
2022-07-22 12:41:29 -0600

    Finished cleaning up code.

--- R/build_min_max_txt.R
+++ R/build_min_max_txt.R
@@ -16,14 +16,16 @@ build_list <- function(f_list, v, period, what) {
 
 build_legend_txt <- function(normals_dir) {
   
-  f_list <- list.files(normals_dir, full.names = T, recursive = TRUE)
+  f_list <- list.files(normals_dir, full.names = T, recursive = TRUE) %>% 
+    grep("quantiles", ., value = T, invert = T) %>% 
+    grep("/maps/", ., value = T, invert = T)
   
   # Iterate over all possibilities and write a json file with each min/max value. 
   tidyr::crossing(
     variables = unique(basename(dirname(f_list))) %>% 
       grep("quantiles|normals", ., invert=T, value = T),
     period = c('annual', tolower(month.abb)),
-    what = c("quantiles", "alpha", "beta", "mean", "median", "mode", "variance")
+    what = c("alpha", "beta", "mean", "median", "mode", "variance")
   ) %>% 
     dplyr::rowwise() %>% 
     dplyr::mutate(j = build_list(f_list, variables, period, what)) %>%
@@ -31,7 +33,7 @@ build_legend_txt <- function(normals_dir) {
       jsonlite::toJSON(
         .$j, auto_unbox = TRUE, pretty = TRUE
       ) %>%
-        jsonlite::write_json(normals_dir)
+        jsonlite::write_json(file.path(normals_dir, "legend_data.json"))
     }
   
 }
--- R/main.R
+++ R/main.R
@@ -1,17 +1,17 @@
+library(magrittr)
 source("./crop_to_roi.R")
 source("./aggregate_period.R")
 source("./normals_slim.R")
+source("./build_min_max_txt.R")
 
 
 # Read in a shapefile of Montana to clip results to.
-roi <- urbnmapr::get_urbn_map(sf = TRUE) %>% 
-  dplyr::filter(state_abbv == 'MT') %>% 
-  sf::st_transform(4326) 
+roi <- mcor::mt_state_simple %>% sf::st_transform(4326)
 
 # define data directories and vairables to use. 
-variables = c("etr", "pet")
-raw_dir = "~/data/gridmet/raw"
-summary_dir = "~/data/gridmet/processed/montana"
+variables = c("erc",  "etr",  "pr",   "rmax", "rmin", "sph",  "srad", "tmmn", "tmmx", "vpd", "vs", "pet")
+raw_dir = "./data/raw"
+summary_dir = "./data/processed/montana"
 
 # Crop the raw gridMET data to the Montana ROI
 crop_to_roi(raw_dir, summary_dir, roi, variables)
@@ -22,10 +22,10 @@ lapply(variables, function(x) {
   aggregate_gridmet(summary_dir, x, "annual")
 })
 
+# Convert ROI to a terra vector object.
+mask <- roi %>% terra::vect()
 
-mask = mcor::mt_state_simple %>% sf::st_transform(4326) %>% terra::vect()
-
-# Calculate Gamma metrics for all variables
+# Calculate and save out Gamma metrics for all variables
 out <- tidyr::crossing(
   variables = variables,
   periods = c("monthly", "annual")
@@ -33,4 +33,7 @@ out <- tidyr::crossing(
   dplyr::rowwise() %>%
   dplyr::mutate(complete = list(iterate_options(variables, periods, summary_dir, mask)))
 
-normals_dir = file.path(summary_dir, "normals")
+
+# Build a json text file for the legend information. 
+normals_dir <- file.path(summary_dir, "normals")
+build_legend_txt(normals_dir)
