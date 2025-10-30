77234ff1546e77013d9c92187ad09ebb19c4e453
colinbrust <colin.brust@gmail.com>
2025-10-30 15:19:21 -0600

    zonal script

--- examples/03_calc_zonal_stats.R
+++ examples/03_calc_zonal_stats.R
@@ -1,10 +1,28 @@
-regions = list(
-  blm = "https://mco-normals.s3.us-east-2.amazonaws.com/fgb/blm.fgb",
-  counties = "https://mco-normals.s3.us-east-2.amazonaws.com/fgb/counties",
-  hucs = "https://mco-normals.s3.us-east-2.amazonaws.com/fgb/hucs.fgb",
-  tribes = "https://mco-normals.s3.us-east-2.amazonaws.com/fgb/tribes.fgb"
-)
 
-data_dir = "~/data/gridmet/montana"
+zonal <- function(v, region) {
+  print(glue::glue("working on {v} or {region}..."))
+  rasts <- terra::rast(glue::glue("~/data/gridmet/montana/{v}/aggregated/") %>%
+                         list.files(full.names = T, pattern = "monthly"))
+  shp = sf::read_sf(glue::glue("https://mco-normals.s3.us-east-2.amazonaws.com/fgb/{region}.fgb"))
 
-list.files(data_dir, full.names = T)
+  names(rasts) <- terra::time(rasts)
+
+  shp <- sf::st_transform(shp, crs = sf::st_crs(rasts))
+  shp_as_rast <- shp %>%
+    terra::vect() %>%
+    terra::rasterize(rasts, field=attr_id)
+
+  out <- terra::zonal(rasts, shp_as_rast, fun=fun, na.rm = T) %>%
+    tibble::as_tibble() %>%
+    tidyr::pivot_longer(-id, names_to = "date")
+
+  readr::write_csv(out, glue::glue("./data/zonal/{region}_{v}.csv"))
+
+}
+
+regions = c("blm", "counties", "hucs", "tribes")
+variables = c("erc", "etr", "pet", "pr", "rmax", "rmin", "sph", "srad", "th", "tmmn", "tmmx", "vpd", "vs")
+
+tidyr::crossing(regions, variables) %>%
+  dplyr::rowwise() %>%
+  dplyr::mutate(out = list(zonal(variables, regions)))
