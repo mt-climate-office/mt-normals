4c62ad920c2b72c0163b9bfb6f89bd990b4786e6
Colin Brust <colin.brust@gmail.com>
2025-12-03 16:26:24 -0700

    Finish all zonal stats and reformat to match file format of existing app

--- examples/prep_cmip_for_s3.R
+++ examples/prep_cmip_for_s3.R
@@ -38,14 +38,6 @@ joined <- dat %>%
       list()
   )
 
-ensemble_mean <- joined %>%
-  dplyr::group_by(scenario, variable) %>%
-  dplyr::summarise(
-    r = list(terra::rast(r) %>% terra::tapp(index = "yearmonths", fun = "mean")),
-    q1 = list(terra::rast(r) %>% terra::tapp(index = "yearmonths", fun = \(x) {quantile(x, 0.1)})),
-    q9 = list(terra::rast(r) %>% terra::tapp(index = "yearmonths", fun = \(x) {quantile(x, 0.9)}))
-  )
-
 zonal <- function(v, rasts, region, scenario, model, shp) {
   print(glue::glue("working on {v} for {region} for {scenario} {model}..."))
   rasts = terra::rotate(rasts)
@@ -58,12 +50,12 @@ zonal <- function(v, rasts, region, scenario, model, shp) {
 
   out <- terra::zonal(rasts, shp_as_rast, fun="mean", na.rm = T) %>%
     tibble::as_tibble() %>%
-    tidyr::pivot_longer(-id, names_to = "date") %>%
-    dplyr::mutate(date = lubridate::as_date(date))
+    tidyr::pivot_longer(-id, names_to = "date")
 
-  readr::write_csv(out, glue::glue("~/data/cmip_tables/{model}_{region}_{v}_{scenario}.csv"))
-
+  # readr::write_csv(out, glue::glue("~/data/cmip_tables/{model}_{region}_{v}_{scenario}.csv"))
+  return(out)
 }
+
 
 regions = tibble::tibble(region=c("blm", "counties", "hucs", "tribes")) %>%
   dplyr::rowwise() %>%
@@ -73,7 +65,57 @@ regions = tibble::tibble(region=c("blm", "counties", "hucs", "tribes")) %>%
 joined %>%
   tidyr::expand_grid(regions) %>%
   dplyr::rowwise() %>%
+  dplyr::filter(variable == 'huss', scenario == 'ssp126') %>%
   dplyr::mutate(summary = list(zonal(variable, r, region, scenario, model, shp)))
+
+
+future::plan(future::multisession, workers = 5)
+
+list.files("~/data/cmip_tables/", full.names = T, pattern = '.csv') %>%
+  tibble::tibble(f = .) %>%
+  dplyr::mutate(meta = basename(f) %>%
+                  tools::file_path_sans_ext()) %>%
+  tidyr::separate(meta, c("model", "type", "variable", "scenario"), sep = "_") %>%
+  dplyr::group_by(type, scenario, variable) %>%
+  dplyr::group_split() %>%
+  purrr::map(function(x) {
+    tmp <- x %>%
+      dplyr::rowwise() %>%
+      dplyr::mutate(dat = list(readr::read_csv(f, show_col_types = F))) %>%
+      tidyr::unnest(dat) %>%
+      dplyr::select(-f)
+
+    if (is.numeric(tmp$date[[1]])) {
+      tmp <- tmp %>%
+        dplyr::mutate(
+          date = date %>%
+            zoo::as.yearmon() %>%
+            zoo::as.Date(frac = 0)
+        )
+    }
+
+    tmp %>%
+      dplyr::group_by(variable, id, year=lubridate::year(date), scenario, model) %>%
+      dplyr::summarise(
+        value = ifelse(unique(variable) %in% c("pr", "penman", "hargreaves", "above90", "con-dry", "con-wet",
+                                "dry-days", "freeze-free", "gdd", "wet-days", "et_m16", "pet_m16",
+                                "gpp", "afgnpp", "pfgnpp", "shrnpp", "trenpp"), sum(value), mean(value))
+      ) %>%
+      dplyr::group_by(year, id, variable, scenario) %>%
+      dplyr::summarise(
+        val = mean(value) %>% round(3),
+        lower = quantile(value, 0.1) %>% round(3),
+        upper = quantile(value, 0.9) %>% round(3)
+      ) %>%
+      dplyr::mutate(plot_type == "timeseries") %>%
+      tidyr::separate(id, c("plot_type", "type", "id", "name"), sep = "_") %>%
+      arrow::write_dataset(
+        "~/data/cmip_zonal",
+        format = "parquet",
+        partitioning = c("type", "id","variable")
+      )
+  }, .progress = TRUE)
+
 
 
 out <- ensemble_mean %>%
@@ -91,6 +133,8 @@ out <- ensemble_mean %>%
         normals::write_as_cog(glue::glue("~/data/cmip_processed/{variable}_{scenario}_annual.tif"))
     )
   )
+
+
 
 # TODO: Create rasters for each variable for baseline, mid century and end of century
 # TODO: Create difference rasters relative to baseline
