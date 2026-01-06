4c0a46f06eb0e8ff443ef15d92faa11446bc6ad2
Kyle Bocinsky <bocinsky@gmail.com>
2026-01-06 15:28:44 -0700

    Initial commit

--- R/globals.R
+++
@@ -1,8 +0,0 @@
-utils::globalVariables(
-  c(".", "d", "month", "year", "out", "season", "geometry", "group_count",
-    "f", "r", "value", "lm", "coef", "value", "climdiv", "scenario", "period",
-    "historical", "model", "ssp_colors", "mid_century", "end_century", "name", "area",
-    "trend", "hist")
-)
-
-sum_vars <- c("above90", "con-dry", "con-wet", "dry-days", "freeze-free", "pr", "wet-days")
